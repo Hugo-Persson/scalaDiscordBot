ff585a2910abae5360045b34c3ceb245b61b5635
Hugo Persson <hugo.e.persson@gmail.com>
2021-10-28 21:16:32 +0200

    init

---
+++ Main.scala
@@ -0,0 +1,8 @@
+import discord4j.core.DiscordClient;
+
+public class MyBot {
+
+  public static void main(String[] args) {
+    DiscordClient client = DiscordClient.create("TOKEN");
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "3.0.1"
+libraryDependencies ++= Seq(
+    "com.discord4j" % "discord4j-core" % "3.2.0"
