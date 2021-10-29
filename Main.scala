d238cf52d635550aa2616f1d33e1d87d0477f466
Hugo Persson <hugo.e.persson@gmail.com>
2021-10-29 23:39:21 +0200

    Started work

--- Main.scala
+++ Main.scala
@@ -1,8 +1,20 @@
-import discord4j.core.DiscordClient;
+import discord4j.core.*;
+import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.`object`.entity.User
+import org.reactivestreams.Publisher
+import scala.compiletime.ops.any
 
-public class MyBot {
+object MyBot {
 
-  public static void main(String[] args) {
-    DiscordClient client = DiscordClient.create("TOKEN");
-  }
+  @main
+  def run = 
+    DiscordClient.create("OTAzMzU1ODY1NTAxOTI5NTUy.YXrxtQ.bgWfT3clJgbXGrXgC114az4sRuw").withGateway(withGateWayHandler).block();
+        
+    
+    def withGateWayHandler(client: GatewayDiscordClient) = 
+      client.on(Class<ReadyEvent>, handleReadyEvent)
+    
+    def handleReadyEvent(event) = 
+      val user: User = event.GetSelf
+  
 }
